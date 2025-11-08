51f7219b12a93d0ac26b65ad976291b4256ec1c0
御坂初琴 <916417956@qq.com>
2025-11-08 18:16:15 +0800

    同步到本地版本

--- 1.稳定版/uninstall.sh
+++ 1.稳定版/uninstall.sh
@@ -1,15 +1,5 @@
 # 这个脚本会在删除模块的时候执行
-# 请不要硬编码 /magisk/modname/... ; 请使用 $MODDIR/...
-# 这将使你的脚本更加兼容，即使Magisk在未来改变了它的挂载点
 MODDIR=${0%/*}
-
-# Enable Cloud Feature By:@Andrower
-chattr -R -i /data/adb/modules/MIUI_MagicWindow+
-chattr -R -i /data/system/cloudFeature_embedded_rules_list.xml
-chattr -R -i /product/etc/embedded_rules_list.xml
-chattr -R -i /data/system/users/0/embedded_setting_config.xml
-
-chcon u:object_r:system_file:s0 $MODDIR/common/product/etc/embedded_rules_list.xml
 
 # Remove Files
 rm /data/system/users/0/magic_window_setting_config.xml
@@ -23,9 +13,9 @@ cp $MODDIR/common/product/etc/embedded_rules_list_bak /data/system/cloudFeature_
 rm /data/system/users/0/embedded_setting_config.xml
 
 # Enable Cloud Feature
-# chmod 660 /product/etc/embedded_rules_list.xml
-# chown system /product/etc/embedded_rules_list.xml
-# chmod 660 /data/system/cloudFeature_embedded_rules_list.xml
-# chown system /data/system/cloudFeature_embedded_rules_list.xml
+chmod 660 /product/etc/embedded_rules_list.xml
+chown system /product/etc/embedded_rules_list.xml
+chmod 660 /data/system/cloudFeature_embedded_rules_list.xml
+chown system /data/system/cloudFeature_embedded_rules_list.xml
 # chmod 660 /data/system/users/0/embedded_setting_config.xml
 # chown system /data/system/users/0/embedded_setting_config.xml
